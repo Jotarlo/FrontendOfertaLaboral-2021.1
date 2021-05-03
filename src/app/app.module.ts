9204a2d8e10258096a974307c054568b61db4af4
Programación Web <programacion.web.misiontic.2022@gmail.com>
2021-05-03 17:40:41 -0500

    Formulario de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MenuLateralComponent } from './publico/paginaMaestra/menu-lateral/menu-lateral.component';
+import { PieDePaginaComponent } from './publico/paginaMaestra/pie-de-pagina/pie-de-pagina.component';
+import { InicioComponent } from './publico/inicio/inicio.component';
+import { BarraNavegacionComponent } from './publico/paginaMaestra/barra-navegacion/barra-navegacion.component';
+import { Error404Component } from './publico/errores/error404/error404.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLateralComponent,
+    PieDePaginaComponent,
+    InicioComponent,
+    BarraNavegacionComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
